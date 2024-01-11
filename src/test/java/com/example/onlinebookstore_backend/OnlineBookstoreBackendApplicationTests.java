1c536a3835276b9b57eea81c6285ba6d912ffa23
sankari <wzr980910@163.com>
2024-01-11 20:07:26 +0800

    提交框架

---
+++ src/test/java/com/example/onlinebookstore_backend/OnlineBookstoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlinebookstore_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBookstoreBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
