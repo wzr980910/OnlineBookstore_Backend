b148898c98661d7002d31800a5f69a2a9773652a
sankari <wzr980910@163.com>
2024-01-12 10:27:05 +0800

    修改目录结构

--- src/main/java/com/bookStore/OnlineBookstoreBackendApplication.java
+++ src/main/java/com/bookStore/OnlineBookstoreBackendApplication.java
@@ -1,4 +1,4 @@
-package com.example.onlinebookstore_backend;
+package com.bookStore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
