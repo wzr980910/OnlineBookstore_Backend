fa0afd6fa62e847e87ed89ee09d3159c84f227e1
3156405145@qq.com <3156405145@qq.com>
2024-01-12 18:35:15 +0800

    第二修改

--- src/main/java/com/bookStore/controller/test.java
+++ src/main/java/com/bookStore/controller/test.java
@@ -1,5 +1,11 @@
 package com.bookStore.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created with Intellij IDEA.
  *
@@ -7,5 +13,15 @@ package com.bookStore.controller;
  * @Date: 2024/01/12/10:42
  * @Description:
  */
+@RestController
+@Tag(name = "测试Controller", description = "这是描述")
 public class test {
+
+    @GetMapping("/hello")
+    @Operation(summary = "测试接口")
+    public String index(@Parameter(name = "name", description = "名称") String name) {
+        return "hello " + name;
+    }
 }
+
+
