3e7c1d94792b666b7e182f44dd573b6890df536e
sankari <wzr980910@163.com>
2024-01-15 09:25:27 +0800

    分离前台前端

--- src/main/java/com/bookStore/config/LoginConfig.java
+++ src/main/java/com/bookStore/config/LoginConfig.java
@@ -1,7 +1,6 @@
 package com.bookStore.config;
 
 import com.bookStore.interceptor.AuthenticationInterceptor;
-import com.bookStore.interceptor.LoginInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
