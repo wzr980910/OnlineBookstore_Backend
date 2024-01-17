cb5c77c7d44fbb0dc18f4beec9b4d0d9caac6ebd
dgc-git <18280349792@163.com>
2024-01-17 10:41:04 +0800

    type book_type实体类生成

--- src/main/java/com/bookStore/mapper/TypeMapper.java
+++ src/main/java/com/bookStore/mapper/TypeMapper.java
@@ -2,15 +2,13 @@ package com.bookStore.mapper;
 
 import com.bookStore.pojo.Type;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.apache.ibatis.annotations.Mapper;
 
 /**
 * @author 邓桂材
 * @description 针对表【type(图书类型表)】的数据库操作Mapper
-* @createDate 2024-01-14 16:56:54
+* @createDate 2024-01-17 10:40:09
 * @Entity com.bookStore.pojo.Type
 */
-@Mapper
 public interface TypeMapper extends BaseMapper<Type> {
 
 }
